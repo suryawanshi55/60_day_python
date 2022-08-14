78a4b0e67ae0e62644223f06b1fcc3701f5b9fc4
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-08-14 11:13:42 +0530

    Add files via upload

---
+++ super+keyword.py
@@ -0,0 +1,11 @@
+# class A:
+#     def method1(self):
+#         print("i'm calling parent class")
+
+# class B(A):
+#     def method1(self):
+#         super().method1()
+#         print("i'm calling child class")
+#         super().method1()         # i'm calling parent class
+# c=B()                             # i'm calling child class
+# c.method1()                       # i'm calling parent class
